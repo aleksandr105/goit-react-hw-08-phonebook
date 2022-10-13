829ac82f848729d65315e6c6f65544f0d977c431
aleksandr105 <87240797+aleksandr105@users.noreply.github.com>
2022-10-13 09:41:52 +0000

    Deploying to gh-pages from @ aleksandr105/goit-react-hw-08-phonebook@a73f0c74858f2f671fcf571d9e4481cd899281da 🚀

---
+++ static/js/765.52702c87.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[765],{9765:function(e,t,c){c.r(t);var r=c(184);t.default=function(){return(0,r.jsx)("section",{children:(0,r.jsx)("h2",{children:"Welcome"})})}}}]);
+//# sourceMappingURL=765.52702c87.chunk.js.map
