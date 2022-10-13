9ce8a02528823262f09987cf48c2337ea00b0630
aleksandr105 <aleksandrsherbina105@gmail.com>
2022-10-13 11:31:49 +0200

    add

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,17 +2,17 @@ import { Routes, Route } from 'react-router-dom';
 import { Layout } from 'components/Layout/Layout';
 import { lazy } from 'react';
 
-const ContactsPage = lazy(() => import('pages/ContactsPage/ContactsPage'));
-const Register = lazy(() => import('pages/Register/Register'));
-const Login = lazy(() => import('pages/Login/Login'));
-const Home = lazy(() => import('pages/Home/Home'));
+const ContactsPage = lazy(() => import('Pages/ContactsPage/ContactsPage'));
+const Register = lazy(() => import('Pages/Register/Register'));
+const Login = lazy(() => import('Pages/Login/Login'));
+// const Home = lazy(() => import('Pages/Home/Home'));
 // const NotFound = lazy(() => import('../../pages/NotFound/NotFound'));
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
+        {/* <Route index element={<Home />} /> */}
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="contacts" element={<ContactsPage />} />
