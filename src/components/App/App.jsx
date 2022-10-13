bed9bbfeeed04d3e4da43a16af0d64ecf292ef21
aleksandr105 <aleksandrsherbina105@gmail.com>
2022-10-13 11:34:30 +0200

    test

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,18 +5,18 @@ import { lazy } from 'react';
 const ContactsPage = lazy(() => import('Pages/ContactsPage/ContactsPage'));
 const Register = lazy(() => import('Pages/Register/Register'));
 const Login = lazy(() => import('Pages/Login/Login'));
-// const Home = lazy(() => import('Pages/Home/Home'));
-// const NotFound = lazy(() => import('../../pages/NotFound/NotFound'));
+const Home = lazy(() => import('Pages/Home/Home'));
+const NotFound = lazy(() => import('../../pages/NotFound/NotFound'));
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        {/* <Route index element={<Home />} /> */}
+        <Route index element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="contacts" element={<ContactsPage />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
